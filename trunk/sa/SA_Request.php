c29c6d5821c1a9710f16cedd68a1d35fef52e09f
andi.trinculescu <andi.trinculescu@ff240404-0450-0410-8b7c-b90bce179856>
2008-06-21 17:47:55 +0000

    fixes

--- trunk/sa/SA_Request.php
+++ trunk/sa/SA_Request.php
@@ -123,9 +123,9 @@ class SA_Request extends SA_Object {
 			$key = trim($params[$i]);
 			if (empty($key)) continue;
 			$value = trim($params[$i + 1]);
-			if (strcasecmp($value, '(:null:)') == 0) $value = null;
+			if (strcasecmp($value, SA_Url::NULL) == 0) $value = null;
 			elseif (is_array($possiblyEncoded = @unserialize(base64_decode($value))) || is_object($possiblyEncoded)) $value = $possiblyEncoded;
-			else $value = str_replace('(:slash:)', '/', $value);
+			else $value = str_replace(SA_Url::SLASH, '/', $value);
 			$_REQUEST[$key] = $_GET[$key] = $value;
 		}
 		$_REQUEST[SA_Application::ACTIONS_VAR_NAME] = $_GET[SA_Application::ACTIONS_VAR_NAME] = explode('-', $_REQUEST[SA_Application::ACTIONS_VAR_NAME]);
